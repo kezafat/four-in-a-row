e1fcc5ea67f48ad981283b3c81058ec7f6809778
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2018-12-04 18:34:50 +0100

    Start activeplayer funktion
    Kommer behöva fixa lite mer på den när fler bitar är på plats

---
+++ www/js/Spelare.js
@@ -0,0 +1,32 @@
+class Spelare {
+    constructor() {
+        this.name = []
+
+    }
+    // Kommer behöva ha klasserna active och player-0/1 
+    // i spelarnas divar. Samt detta som körs en gång när
+    //spelet startar sen anropas nextPlayer efter varje drag så byts spelaren
+
+    // $('.player-0').classList.remove('active');
+    // $('.player-1').classList.remove('active');
+    //$('.player-0').classList.add('active');
+    // activePlayer = this.name[0]
+    // plus lite styling i sass för klassen 'active'
+
+    nextPlayer() {
+        if (activePlayer == this.name[0]) {
+            activePlayer = this.name[1];
+            $('.player-0').classList.toggle('active');
+            $('.player-1').classList.toggle('active');
+        }
+        else if (activePlayer == this.name[1]) {
+            activePlayer = this.name[0];
+            $('.player-0').classList.toggle('active');
+            $('.player-1').classList.toggle('active');
+        }
+        else {
+
+        }
+    }
+}
+
