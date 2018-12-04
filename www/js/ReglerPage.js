b705604e71bfdc3b6a275821c242b9b87e47ec6f
SimonPalmgren <simon.palmgren91@gmail.com>
2018-12-04 17:40:12 +0100

    changed this.name

--- www/js/ReglerPage.js
+++ www/js/ReglerPage.js
@@ -2,7 +2,7 @@ class ReglerPage extends Component{
   constructor() {
     super();
     this.addRoute('/spelregler', 'Spelregler');
-    this.name = "Regler";
+    this.name = "Spelregler";
   }
 
 }
