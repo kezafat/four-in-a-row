5794118a2be6446bc32a5502b182c6714204958d
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2018-12-06 13:38:46 +0100

    Början av namechecker

--- www/js/SpelaPage.js
+++ www/js/SpelaPage.js
@@ -2,11 +2,27 @@ class SpelaPage extends Component {
   constructor() {
     super();
     this.addRoute('/spela', 'Spela');
+    this.addEvents({
+      'click .start-btn': 'checkName'
+    });
     this.name = "Spelapage";
     this.players = [];
   }
-  checkname() {
-    let player = $('player-1-name').val();
-    this.players.push(new Spelare(playerName, this));
+  checkName() {
+
+    let playerName = $('.player-0-name').val();
+    if (playerName > 2 && playerName < 10) {
+      this.players.push(new Spelare(playerName));
+    }
+    else {
+      $('.player-0-h2').text('Namnet måste vara mellan två och åtta bokstäver');
+    }
+    playerName = $('.player-1-name').val();
+    if (playerName > 2 && playerName < 10) {
+      this.players.push(new Spelare(playerName));
+    }
+    else {
+      $('.player-1-h2').text('Namnet måste vara mellan två och åtta bokstäver');
+    }
   }
 }
--- www/js/Spelare.js
+++ www/js/Spelare.js
@@ -1,32 +1,39 @@
 class Spelare {
-    constructor() {
+	constructor(playerName) {
+		playerName = [];
+		[] = playerName;
 
+	}
 
-    }
-    // Kommer behöva ha klasserna active och player-0/1 
-    // i spelarnas divar. Samt detta som körs en gång när
-    //spelet startar sen anropas nextPlayer efter varje drag så byts spelaren
+	addPlayer() {
 
-    // $('.player-0').classList.remove('active');
-    // $('.player-1').classList.remove('active');
-    //$('.player-0').classList.add('active');
-    // activePlayer = this.name[0]
-    // plus lite styling i sass för klassen 'active'
+	}
 
-    nextPlayer() {
-        if (activePlayer == this.playerName[0]) {
-            activePlayer = this.playerName[1];
-            $('.player-0').classList.toggle('active');
-            $('.player-1').classList.toggle('active');
-        }
-        else if (activePlayer == this.playerName[1]) {
-            activePlayer = this.playerName[0];
-            $('.player-0').classList.toggle('active');
-            $('.player-1').classList.toggle('active');
-        }
-        else {
+	// Kommer behöva ha klasserna active och player-0/1 
+	// i spelarnas divar. Samt detta som körs en gång när
+	//spelet startar sen anropas nextPlayer efter varje drag så byts spelaren
 
-        }
-    }
+	// $('.player-0').classList.remove('active');
+	// $('.player-1').classList.remove('active');
+	//$('.player-0').classList.add('active');
+	// activePlayer = this.name[0]
+	// plus lite styling i sass för klassen 'active'
+	nextPlayer() {
+		if (activePlayer == this.player[0]) {
+			activePlayer = this.player[1];
+			$('.player-0').classList.toggle('active');
+			$('.player-1').classList.toggle('active');
+		}
+		else if (activePlayer == this.player[1]) {
+			activePlayer = this.player[0];
+			$('.player-0').classList.toggle('active');
+			$('.player-1').classList.toggle('active');
+		}
+		else {
+			console.log("2ad")
+		}
+	}
+
 }
+
 
