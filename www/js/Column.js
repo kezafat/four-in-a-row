123df23e8b411f01fc74bee84894218073ce98d9
Hamsho <hamidjade@gmail.com>
2018-12-10 14:38:47 +0100

    Fixed indentations and removed excess comments
    Sloppy sloppy!

--- www/js/Column.js
+++ www/js/Column.js
@@ -1,27 +1,16 @@
-class Column extends Component{ 
+class Column extends Component {
 
-    constructor(cNum){
-        super();
-        this.cNum = cNum;
-        this.cells = [];
-        
-        this.makeCells();
-        
+  constructor(cNum) {
+    super();
+    this.cNum = cNum;
+    this.cells = [];
+    this.makeCells();
+  }
+
+  makeCells() {
+    for (let i = 1; i <= 6; i++) {
+      this.cells.push(new Cell(this.cNum, i));
     }
-
-    makeCells() {
-        
-        for(let i = 1; i <= 6; i++){
-
-                this.cells.push(new Cell( this.cNum, i));
-
-                
-            }
-
-            
-           
-        }
-
-       
-    }
+  }
+}
 
