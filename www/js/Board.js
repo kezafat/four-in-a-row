3f35328098e7c1798fd505744175a2a4103c8318
Ammar <hamedi.email@gmail.com>
2018-12-11 12:48:40 +0100

    fixed emty  line

--- www/js/Board.js
+++ www/js/Board.js
@@ -2,27 +2,17 @@ class Board extends Component{
     
     constructor(){
         super();
-
-
         this.columns = [];
         
         this.makeColumns();
         
     }
-
     makeColumns() {
         
         for(let i = 1; i <= 7; i++){
-
-            // for(let j = 1; j < 7; j++){
             this.columns.push(new Column(i));
-
-            
-            // }
         }
 
-        console.log(this.columns)
-
+        console.log(this.columns);
     }
-
 }
