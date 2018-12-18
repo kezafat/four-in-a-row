dc191c0c663ef579b045b2362679442417fb7be4
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2018-12-18 19:39:40 +0100

    Bot player
    Player can now choose bot and added player type in the player page.

--- www/js/SpelaPage.js
+++ www/js/SpelaPage.js
@@ -19,12 +19,17 @@ class SpelaPage extends Component {
   updatePlayerPage() {
     this.gameMode = false;
     this.players = [];
+    this.tmpName0 = '';
+    this.tmpName1 = '';
     this.render();
   }
 
   checkName() {
     let playerName0 = $('.player-0-name').val();
     let playerName1 = $('.player-1-name').val();
+
+    this.playerType0 = $('input[name=player-0-type]:checked').val();
+    this.playerType1 = $('input[name=player-1-type]:checked').val();
 
     this.tmpName0 = playerName0;
     this.tmpName1 = playerName1;
@@ -49,10 +54,22 @@ class SpelaPage extends Component {
       this.validate1 = false;
       this.render();
     }
+    if (this.playerType0 === "human") {
+      this.tmpName0 += " 🤓";
+    }
+    else {
+      this.tmpName0 += " 🤖";
+    }
+    if (this.playerType1 === "human") {
+      this.tmpName1 += " 🤓";
+    }
+    else {
+      this.tmpName1 += " 🤖";
+    }
 
     if (validated0 && validated1) {
-      this.players.push(new Player(playerName0, 0));
-      this.players.push(new Player(playerName1, 1));
+      this.players.push(new Player(playerName0, 0, this.playerType0));
+      this.players.push(new Player(playerName1, 1, this.playerType1));
       this.gameMode = true;
     }
   }
