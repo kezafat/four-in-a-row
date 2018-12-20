4d38383ee5e760b18bc8f4831bca184ceb4bec80
Hamsho <hamidjade@gmail.com>
2018-12-20 09:03:20 +0100

    Firefox bug: fixed
    Missing render call added

--- www/js/SpelaPage.js
+++ www/js/SpelaPage.js
@@ -10,8 +10,8 @@ class SpelaPage extends Component {
     this.players = [];
     this.validate0 = true;
     this.validate1 = true;
-    this.tmpName0 = 'Hamid';
-    this.tmpName1 = 'Daniel';
+    this.tmpName0 = '';
+    this.tmpName1 = '';
     this.gameMode = false;
     this.board = new Board(this);
   }
@@ -73,6 +73,7 @@ class SpelaPage extends Component {
       this.players.push(new Player(playerName0, 0, this.playerType0));
       this.players.push(new Player(playerName1, 1, this.playerType1));
       this.gameMode = true;
+      this.render();
     }
   }
 
