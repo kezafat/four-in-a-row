e7821520c2b2b7e6f58643d4fba5637ca9260233
SimonPalmgren <simon.palmgren91@gmail.com>
2019-01-05 13:56:54 +0100

    fixed so you dont have to hard refresh site

--- www/js/TopplistaPage.js
+++ www/js/TopplistaPage.js
@@ -3,7 +3,15 @@ class TopplistaPage extends Component{
     super();
     this.addRoute('/topplista', 'Topplistan');
     this.name = "Topplista";
-    this.topplistaPageBoard = new TopplistaPageBoard();
-    let that = this;
+	this.topplistaPageBoard=new TopplistaPageBoard();
+	let that = this;
+	this.refresh=function(){
+	  that.topplistaPageBoard.makeRows();
+	}
+	//refreshTopplista
+	this.addEvents({
+      'click .refreshTopplista': 'refresh'
+    });
   }
+  
 }
